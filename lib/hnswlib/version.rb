b4e193ffb7b163b3de877c81352057d036a721d4
yoshoku <yoshoku@outlook.com>
2021-07-24 17:13:12 +0900

    :books: Add API documentation

--- lib/hnswlib/version.rb
+++ lib/hnswlib/version.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+# Hnswlib.rb provides Ruby bindings for the Hnswlib.
 module Hnswlib
+  # The version of Hnswlib.rb you install.
   VERSION = '0.1.0'
+  # The version of Hnswlib included with gem.
   HSWLIB_VERSION = '0.5.2'
 end
