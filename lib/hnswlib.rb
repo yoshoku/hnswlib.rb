48365a8ed558258747dded9bb94794b9ec9e6089
yoshoku <yoshoku@outlook.com>
2021-07-24 13:55:26 +0900

    :sparkles: Add Index class

--- lib/hnswlib.rb
+++ lib/hnswlib.rb
@@ -4,6 +4,144 @@ require_relative 'hnswlib/version'
 require_relative 'hnswlib/hnswlibext'
 
 module Hnswlib
-  class Error < StandardError; end
-  # Your code goes here...
+  # Index is a class that provides functions for k-nearest eighbors search.
+  #
+  # @example
+  #   require 'hnswlib'
+  #
+  #   index = Hnswlib::Index.new(n_features: 100, max_item: 10000)
+  #
+  #   5000.times do |item_id|
+  #     item_vec = Array.new(100) { rand - 0.5 }
+  #     index.add_item(item_id, item_vec)
+  #   end
+  #
+  #   index.get_nns_by_item(0, 100)
+  #
+  class Index
+    # Returns the metric of index.
+    # @return [String]
+    attr_reader :metric
+
+    # Create a new search index.
+    #
+    # @param n_features [Integer] The number of features (dimensions) of stored vector.
+    # @param max_item [Integer] The maximum number of items.
+    # @param metric [String] The distance metric between vectors ('l2' or 'dot').
+    # @param m [Integer] The maximum number of outgoing connections in the graph
+    # @param ef_construction [Integer] The size of the dynamic list for the nearest neighbors. It controls the index time/accuracy trade-off.
+    # @param random_seed [Integer] The seed value using to initialize the random generator.
+    def initialize(n_features:, max_item:, metric: 'l2', m: 16, ef_construction: 200, random_seed: 100)
+      @metric = metric
+      space = if @metric == 'dot'
+                Hnswlib::InnerProductSpace.new(n_features)
+              else
+                Hnswlib::L2Space.new(n_features)
+              end
+      @index = Hnswlib::HierarchicalNSW.new(
+        space: space, max_elements: max_item, m: m, ef_construction: ef_construction, random_seed: random_seed
+      )
+    end
+
+    # Add item to be indexed.
+    #
+    # @param i [Integer] The ID of item.
+    # @param v [Array] The vector of item.
+    # @return [Boolean]
+    def add_item(i, v)
+      @index.add_point(v, i)
+    end
+
+    # Return the item vector.
+    #
+    # @param i [Integer] The ID of item.
+    # @return [Array]
+    def get_item(i)
+      @index.get_point(i)
+    end
+
+    # Remove the item vector.
+    #
+    # @param i [Integer] The ID of item.
+    # @return [Array]
+    def remove_item(i)
+      @index.mark_deleted(i)
+    end
+
+    # Search the n closest items.
+    #
+    # @param i [Integer] The ID of query item.
+    # @param n [Integer] The number of nearest neighbors.
+    # @param include_distances [Boolean] The flag indicating whether to returns all corresponding distances.
+    # @return [Array<Integer> or Array<Array<Integer>, Array<Float>>]
+    def get_nns_by_item(i, n, include_distances: false)
+      v = @index.get_point(i)
+      ids, dists = @index.search_knn(v, n)
+      include_distances ? [ids, dists] : ids
+    end
+
+    # Search the n closest items.
+    #
+    # @param v [Array] The vector of query item.
+    # @param n [Integer] The number of nearest neighbors.
+    # @param include_distances [Boolean] The flag indicating whether to returns all corresponding distances.
+    # @return [Array<Integer> or Array<Array<Integer>, Array<Float>>]
+    def get_nns_by_vector(v, n, include_distances: false)
+      ids, dists = @index.search_knn(v, n)
+      include_distances ? [ids, dists] : ids
+    end
+
+    # Reize the search index.
+    #
+    # @param new_max_item [Integer] The maximum number of items.
+    def resize_index(new_max_item)
+      @index.reisze_index(new_max_item)
+    end
+
+    # Save the search index to disk.
+    #
+    # @param filename [String] The filename of search index.
+    def save(filename)
+      @index.save_index(filename)
+    end
+
+    # Load a search index from disk.
+    #
+    # @param filename [String] The filename of search index.
+    def load(filename)
+      @index.load_index(filename)
+    end
+
+    # Calculate the distances between items.
+    #
+    # @param i [Integer] The ID of item.
+    # @param j [Integer] The ID of item.
+    # @return [Float or Integer]
+    def get_distance(i, j)
+      vi = @index.get_point(i)
+      vj = @index.get_point(j)
+      @index.space.distance(vi, vj)
+    end
+
+    # Return the number of items in the search index.
+    #
+    # @return [Integer]
+    def n_items
+      @index.current_count
+    end
+
+    # Returns the number of features of indexed item.
+    #
+    # @return [Integer]
+    def n_features
+      @index.space.dim
+    end
+
+    # Return the maximum number of items.
+    #
+    # @return [Integer]
+    def max_item
+      @index.max_elements
+    end
+  end
 end
--- spec/hnswlib_spec.rb
+++ spec/hnswlib_spec.rb
@@ -1,7 +1,138 @@
 # frozen_string_literal: true
 
 RSpec.describe Hnswlib do
-  it 'has a version number' do
-    expect(Hnswlib::VERSION).not_to be nil
+  describe Hnswlib::Index do
+    let(:n_features) { 4 }
+    let(:metric) { 'l2' }
+    let(:n_items) { 5 }
+    let(:max_item) { n_items * 2 }
+    let(:n_neighbors) { 3 }
+    let(:index) { described_class.new(n_features: n_features, max_item: max_item, metric: metric, random_seed: 84) }
+
+    before do
+      index.add_item(0, [0, 1, 2, 3])
+      index.add_item(1, [1, 1, 2, 3])
+      index.add_item(2, [1, 1, 2, 4])
+      index.add_item(3, [2, 1, 2, 5])
+      index.add_item(4, [2, 1, 2, 6])
+    end
+
+    describe '#get_nns_by_item' do
+      context 'when include_distances is false' do
+        subject { index.get_nns_by_item(0, n_neighbors) }
+
+        it 'returns id list of nearest neighbors' do
+          expect(subject).to match([0, 1, 2])
+        end
+      end
+
+      context 'when include_distances is true' do
+        subject { index.get_nns_by_item(0, n_neighbors, include_distances: true) }
+
+        it 'returns id list and distances of nearest neighbors' do
+          expect(subject).to match([[0, 1, 2], [0, 1, 2]])
+        end
+      end
+    end
+
+    describe '#get_nns_by_vector' do
+      let(:query) { [0, 1, 2, 3] }
+
+      context 'when include_distances is false' do
+        subject { index.get_nns_by_vector(query, n_neighbors) }
+
+        it 'returns id list of nearest neighbors' do
+          expect(subject).to match([0, 1, 2])
+        end
+      end
+
+      context 'when include_distances is true' do
+        subject { index.get_nns_by_vector(query, n_neighbors, include_distances: true) }
+
+        it 'returns id list and distances of nearest neighbors' do
+          expect(subject).to match([[0, 1, 2], [0, 1, 2]])
+        end
+      end
+    end
+
+    describe '#get_item' do
+      subject { index.get_item(0) }
+
+      it 'returns the item vector specified by ID' do
+        expect(subject).to match([0, 1, 2, 3])
+      end
+    end
+
+    describe '#n_fetures' do
+      subject { index.n_features }
+
+      it 'returns the number of features' do
+        expect(subject).to eq(n_features)
+      end
+    end
+
+    describe '#metric' do
+      subject { index.metric }
+
+      it 'returns the distance metric' do
+        expect(subject).to eq(metric)
+      end
+    end
+
+    describe '#n_items' do
+      subject { index.n_items }
+
+      it 'returns the number of indexed items' do
+        expect(subject).to eq(n_items)
+      end
+    end
+
+    describe '#max_item' do
+      subject { index.max_item }
+
+      it 'returns the maximum number of items' do
+        expect(subject).to eq(max_item)
+      end
+    end
+
+    describe '#save and #load' do
+      let(:filename) { File.expand_path("#{__dir__}/test.ann") }
+
+      before { index.save(filename) }
+
+      it 'load the saved index', :aggregate_failures do
+        loaded = described_class.new(n_features: n_features, metric: metric, max_item: n_items)
+        loaded.load(filename)
+        expect(loaded.n_items).to eq(index.n_items)
+        expect(loaded.get_item(0)).to match(index.get_item(0))
+      end
+    end
+
+    describe '#get_distance' do
+      subject { index.get_distance(0, 1) }
+
+      before do
+        index.add_item(0, [1, 0, 1, 2])
+        index.add_item(1, [3, 1, 1, 2])
+      end
+
+      context "when metric is 'dot'" do
+        let(:metric) { 'dot' }
+
+        it 'returns 1 - dot product between items' do
+          # 1 - (1 * 3 + 0 * 1 + 1 * 1 + 2 * 2)
+          expect(subject).to be_within(1e-8).of(-7)
+        end
+      end
+
+      context "when metric is 'l2'" do
+        let(:metric) { 'l2' }
+
+        it 'returns squared error between items' do
+          # (1 - 3)**2 + (0 - 1)**2 + (1 - 1)**2 + (2 - 2)**2
+          expect(subject).to be_within(1e-8).of(5)
+        end
+      end
+    end
   end
 end
