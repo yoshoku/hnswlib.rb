22bfa09ae3d27720251fd49afcf8f429d961d994
yoshoku <yoshoku@outlook.com>
2022-01-29 10:03:16 +0900

    test: add loading test for index created with python

--- spec/hnswlib_spec.rb
+++ spec/hnswlib_spec.rb
@@ -96,15 +96,63 @@ RSpec.describe Hnswlib do
     end
 
     describe '#save and #load' do
-      let(:filename) { File.expand_path("#{__dir__}/test.ann") }
+      context 'with an index created by hnswlib.rb' do
+        let(:filename) { File.expand_path("#{__dir__}/test.ann") }
 
-      before { index.save(filename) }
+        before { index.save(filename) }
 
-      it 'load the saved index', :aggregate_failures do
-        loaded = described_class.new(n_features: n_features, metric: metric, max_item: n_items)
-        loaded.load(filename)
-        expect(loaded.n_items).to eq(index.n_items)
-        expect(loaded.get_item(0)).to match(index.get_item(0))
+        it 'load the saved index', :aggregate_failures do
+          loaded = described_class.new(n_features: n_features, metric: metric, max_item: n_items)
+          loaded.load(filename)
+          expect(loaded.n_items).to eq(index.n_items)
+          expect(loaded.get_item(0)).to match(index.get_item(0))
+        end
+      end
+
+      context 'with an index created by the Python bindings' do
+        # import hnswlib
+        # import numpy as np
+        #
+        # f = 4
+        # n = 8
+        #
+        # data = np.float32([
+        #   [0, 1, 2, 3],
+        #   [1, 2, 3, 4],
+        #   [2, 3, 4, 5],
+        #   [3, 4, 5, 6],
+        #   [4, 5, 6, 7],
+        #   [5, 6, 7, 8],
+        #   [6, 7, 8, 9],
+        #   [7, 8, 9, 0]
+        # ])
+        # ids = np.arange(n)
+        #
+        # p = hnswlib.Index(space='l2', dim=f)
+        # p.init_index(max_elements=n)
+        #
+        # p.add_items(data, ids)
+        # p.save_index('pytest.ann')
+        let(:filename) { File.expand_path("#{__dir__}/pytest.ann") }
+
+        it 'load the saved index', :aggregate_failures do
+          loaded = described_class.new(n_features: 4, metric: metric, max_item: 8)
+          loaded.load(filename)
+          expect(loaded.n_items).to eq(8)
+          expect(loaded.n_features).to eq(4)
+          expect(loaded.metric).to eq('l2')
+          expect(loaded.get_item(0)).to match([0, 1, 2, 3])
+          expect(loaded.get_item(1)).to match([1, 2, 3, 4])
+          expect(loaded.get_item(2)).to match([2, 3, 4, 5])
+          expect(loaded.get_item(3)).to match([3, 4, 5, 6])
+          expect(loaded.get_item(4)).to match([4, 5, 6, 7])
+          expect(loaded.get_item(5)).to match([5, 6, 7, 8])
+          expect(loaded.get_item(6)).to match([6, 7, 8, 9])
+          expect(loaded.get_item(7)).to match([7, 8, 9, 0])
+          expect(loaded.get_nns_by_item(0, 4, include_distances: true)).to match(
+            [[0, 1, 2, 3], [0.0, 4.0, 16.0, 36.0]]
+          )
+        end
       end
     end
 
