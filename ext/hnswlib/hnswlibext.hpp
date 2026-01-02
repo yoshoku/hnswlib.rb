71af73aaaadec2daa9a4240fa6e08383ed4f6e1e
yoshoku <yoshoku@outlook.com>
2026-01-02 10:48:02 +0900

    docs: update copyright year

--- ext/hnswlib/hnswlibext.hpp
+++ ext/hnswlib/hnswlibext.hpp
@@ -1,7 +1,7 @@
 /**
  * hnswlib.rb is a Ruby binding for the Hnswlib.
  *
- * Copyright (c) 2021-2025 Atsushi Tatsuma
+ * Copyright (c) 2021-2026 Atsushi Tatsuma
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
